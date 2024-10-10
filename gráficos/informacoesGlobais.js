3839f60d6b9d67544f642043c94088bb95dd6c78
ericakaroline302007 <111869443+ericakaroline302007@users.noreply.github.com>
2024-10-10 07:52:11 -0300

    gbn

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -18,6 +18,9 @@ async function visualizarInformacoesGlobais() {
         const paragrafo = document. createElement ('p')
         paragrafo.classList.add ('graficos-container texto')
         paragrafo. innerHTML = `Você sabia que o mundo tem
-        §{dados. total pessoas mundo} de pessoas e que aproximadamente ${dados. total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média
-        §{dados. tempo_medio} horas conectadas.`
+        ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média
+        ${dados.tempo_medio} horas conectadas.`
         console. log (paragrafo)
+        const container = document.getElementById(' graficos-container')
+            container.appendChild(paragrafo)
+    }
